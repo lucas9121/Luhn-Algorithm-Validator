26be6e8aab13934a3e8a60699211729887ecf753
Lucas <lucas2carlos@gmail.com>
2022-04-06 20:05:01 -0400

    building index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import { StrictMode } from "react";
+import ReactDOM from "react-dom"
+import "./style.css"
+import App from "./App"
+
+ReactDOM.render(
+    <StrictMode>
+        <App />
+    </StrictMode>,
+    document.getElementById('root')
+)
